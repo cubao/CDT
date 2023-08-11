7d1c42c9f99b4772bdb99a4d1f97c29e3085d5e6
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-08-11 19:50:01 +0800

    bindings

--- cdt_bindings.cpp
+++ cdt_bindings.cpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// https://github.com/artem-ogre/PythonCDT/blob/master/cdt_bindings.cpp
+
 #include <CDT.h>
 #include <VerifyTopology.h>
 
